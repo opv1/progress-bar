5d46670ffe4301868399b39c6ac66a983904c546
opv1 <pas7273@gmail.com>
2022-09-12 21:57:48 +0300

    more beauty

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { AppProvider } from 'context/AppState'
 import App from 'App'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <AppProvider>
     <App />
   </AppProvider>,
-  document.getElementById('root'),
 )
